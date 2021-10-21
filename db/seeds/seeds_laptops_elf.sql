2adbe703697f941afd646ded57013bde8e4802c7
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-10-21 13:53:31 +0000

    implement login form and db

---
+++ db/seeds/seeds_laptops_elf.sql
@@ -0,0 +1,8 @@
+INSERT INTO laptops (
+  id, brand_name, screen_size, condition, memory, price, storage_size, storage_type)
+  VALUES (1, 'Lenovo', 15, 'like new', 512, 10000, 8, 'SSD')
+
+
+INSERT INTO laptops (
+  id, brand_name, screen_size, condition, memory, price, storage_size, storage_type)
+  VALUES (2, 'Microsoft', 12.4, 'like new', 1035, 10000, 8, 'RAM')
