abad8cd04607d90c791fae004eed16cc7ef6096c
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-10-21 14:54:47 +0000

    apdate schima

--- db/seeds/seeds_users_elf.sql
+++ db/seeds/seeds_users_elf.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (
-  id, name, email, password, contact_phone, photo_url)
-VALUES (1,'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '7005001234', NULL);
+  id, name, email, password, contact_phone, photo_url, is_admin)
+VALUES (1,'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '7005001234', NULL, FALSE);
 
 INSERT INTO users (
-  id, name, email, password, contact_phone, photo_url) 
-  VALUES (2,'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '7115001243', NULL);
+  id, name, email, password, contact_phone, photo_url, is_admin) 
+  VALUES (2,'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '7115001243', NULL, TRUE);
